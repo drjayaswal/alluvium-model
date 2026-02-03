4a8fd55ce73693d797060d81c379ad1106b82b43
Dhruv Ratan Jayaswal <mrdhruv.professional@gmail.com>
2026-02-03 12:20:13 +0530

    patch x3

--- app/main.py
+++ app/main.py
@@ -44,7 +44,6 @@ async def analyze_s3(data: dict):
             s3_client.delete_object(Bucket=bucket_name, Key=s3_key)
         except Exception as delete_err:
             raise Exception({"message":delete_err})
-        print(results)
         return results
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
@@ -73,7 +72,6 @@ async def analyze_drive(data: dict):
                  raise Exception("File content too small; likely a failed download.")
             resume_text = extract.text(resp.content, target_mime)
         results = await process(resume_text, description, filename)
-        print(results)
         return results
         
     except Exception as e:
